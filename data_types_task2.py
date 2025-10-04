0ca9763cc8fc0f8197f498a4439fa1bf4d2c09fe
MeetToast5578 <alizadeyunus@gmail.com>
2025-10-04 19:15:04 +0400

    people = [     {'name': 'Alice', 'age': 30, 'city': 'New York'},     {'name': 'Bob', 'age': 22, 'city': 'San Francisco'},     {'name': 'Charlie', 'age': 35, 'city': 'Los Angeles'} ] age = [] print("Names of people older than 25:") for item in people:     age.append(item.get("age"))     if item.get("age") > 25:         print(item.get("name")) print("""       """) print("Sorted by age (descending):") for i in reversed(sorted(age)):     for item in people:         if item.get("age") == i:             print(item)             people.remove(item)

---
+++ data_types_task2.py
@@ -0,0 +1,21 @@
+# n = int(input("number of entries: "))
+# people = [{"name": input("name: "), "age": int(input("age: ")), "city": input("city: ")} for i in range(n)]
+people = [
+    {'name': 'Alice', 'age': 30, 'city': 'New York'},
+    {'name': 'Bob', 'age': 22, 'city': 'San Francisco'},
+    {'name': 'Charlie', 'age': 35, 'city': 'Los Angeles'}
+]
+age = []
+print("Names of people older than 25:")
+for item in people:
+    age.append(item.get("age"))
+    if item.get("age") > 25:
+        print(item.get("name"))
+print("""
+      """)
+print("Sorted by age (descending):")
+for i in reversed(sorted(age)):
+    for item in people:
+        if item.get("age") == i:
+            print(item)
+            people.remove(item)
